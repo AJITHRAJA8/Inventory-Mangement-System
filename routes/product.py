823d50dff45387bf11be7e79edb5de225324c929
AJITHRAJA8 <ajithrajar79@gmail.com>
2026-07-04 12:39:13 +0530

    add_product page was developed

--- routes/product.py
+++ routes/product.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request
 from database import con
+import os
+import uuid
+from werkzeug.utils import secure_filename
 from helpers import fetch_all_dict, fetch_one_dict
 import math
 
@@ -137,3 +140,197 @@ def product():
         total_pages=total_pages,
         search=search
     )
+
+# ==========================================
+# Add Product
+# ==========================================
+@product_bp.route("/add_product", methods=["GET", "POST"])
+def add_product():
+
+    if "user" not in session:
+        return redirect(url_for("auth.login"))
+
+    # ======================================
+    # Load Categories
+    # ======================================
+
+    res = con.cursor()
+
+    sql = """
+    SELECT
+        category_id,
+        category_name
+    FROM category
+    ORDER BY category_name
+    """
+
+    res.execute(sql)
+
+    categories = fetch_all_dict(res)
+
+    # ======================================
+    # Load Suppliers
+    # ======================================
+
+    res = con.cursor()
+
+    sql = """
+    SELECT
+        supplier_id,
+        supplier_name
+    FROM supplier
+    ORDER BY supplier_name
+    """
+
+    res.execute(sql)
+
+    suppliers = fetch_all_dict(res)
+
+    # ======================================
+    # Save Product
+    # ======================================
+
+    if request.method == "POST":
+
+        product_name = request.form["product_name"]
+        category_id = request.form["category_id"]
+        supplier_id = request.form["supplier_id"]
+        price = request.form["price"]
+        stock = request.form["stock"]
+        description = request.form["description"]
+
+        # Image Upload
+        image = request.files["image"]
+
+        filename = "no-image.png"
+
+        if image and image.filename != "":
+
+            extension = os.path.splitext(image.filename)[1]
+
+            filename = str(uuid.uuid4()) + extension
+
+            image.save(
+                os.path.join(
+                    "static/images",
+                    filename
+                )
+            )
+
+        # ==============================
+        # Duplicate Product Check
+        # ==============================
+
+        res = con.cursor()
+
+        sql = """
+        SELECT *
+        FROM product
+        WHERE product_name = ?
+        """
+
+        res.execute(sql, (product_name,))
+
+        product = fetch_one_dict(res)
+
+        if product:
+
+            return render_template(
+                "add_product.html",
+                error="Product already exists.",
+                product_name=product_name,
+                categories=categories,
+                suppliers=suppliers
+            )
+
+        # ==============================
+        # Insert Product
+        # ==============================
+
+        res = con.cursor()
+
+        sql = """
+        INSERT INTO product
+        (
+            product_name,
+            category_id,
+            supplier_id,
+            price,
+            stock,
+            image,
+            description
+        )
+        VALUES
+        (?, ?, ?, ?, ?, ?, ?)
+        """
+
+        value = (
+            product_name,
+            category_id,
+            supplier_id,
+            price,
+            stock,
+            filename,
+            description
+        )
+
+        res.execute(sql, value)
+
+        con.commit()
+
+        flash("Product added successfully.", "success")
+
+        return redirect(url_for("product.product"))
+
+    # ======================================
+    # Load Page
+    # ======================================
+
+    return render_template(
+        "add_product.html",
+        categories=categories,
+        suppliers=suppliers
+    )
+
+#==================
+# Update prouduct
+#==================
+@product_bp.route('/update_product/<int:product_id>',methods=['GET','POST'])
+def update_product():
+
+    if 'user' not in session:
+        return redirect('auth.login')
+    
+    # ======================================
+    # Load Categories
+    # ======================================
+
+    res = con.cursor()
+
+    sql = 'select category_id,category_name from category' \
+    ' order by category_name'
+
+    res.execute(sql)
+
+    categories = fetch_all_dict(res)
+
+    # ======================================
+    # Load Suppliers
+    # ======================================
+    res = con.cursor()
+
+    sql = 'select supplier_id,supplier_name from suppliers' \
+    'order by supplier_name'
+
+    res.execute(sql)
+
+    suppliers = fetch_all_dict(res)
+
+    # ======================================
+    # Save Product
+    # ======================================
+
+
+
+
+    
